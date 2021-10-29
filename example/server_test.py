b6fa0db3e617de116c01445d709c3cd8fe1e1ab5
陈品珍 <549471729@qq.com>
2021-10-29 12:31:25 +0800

    feat: 添加定时器

--- example/server_test.py
+++ example/server_test.py
@@ -3,14 +3,27 @@ import server
 from net import msghandler
 from net.connmanager import Request, Response
 
+from util import timer
+
 
 def ping_view(request: Request):
     request.conn.send_response(Response(2, b'response body'))
 
 
+# 注册路由
 msghandler.register_route(1, ping_view)
 
+# 断开连接的回调
 server.set_connection_lost(lambda d: print("offline callback.", d))
+
+
+# 测试的发送数据给所有的连接
+def sendto_all():
+    server.factory.conn_manager.sendto_all(Response(1, b'pong!!!'))
+
+
+# 定时循环
+timer.add_loop_task(3, sendto_all, now=False)
 
 
 if __name__ == '__main__':
---
+++ util/atomic_counter.py
@@ -0,0 +1,12 @@
+import threading
+
+
+class AtomicCounter:
+    def __init__(self, initial=0):
+        self.value = initial
+        self._lock = threading.Lock()
+
+    def increment(self, num=1):
+        with self._lock:
+            self.value += num
+            return self.value
