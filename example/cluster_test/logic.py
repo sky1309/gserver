dfdd8edc57a15c4489712e9ff947171aa17e9d27
陈品珍 <549471729@qq.com>
2021-11-23 21:38:04 +0800

    test: gate和logic服务之间的通讯测试样例

--- example/cluster_test/logic.py
+++ example/cluster_test/logic.py
@@ -7,14 +7,22 @@ sv = service.Service("logic")
 cluster.cluster.pb_server.set_service(sv)
 
 
-@sv.route("foo")
-def foo(name):
-    print("logic foo func", name)
+@sv.route(1)
+def foo(nodeid, sessionid, data):
+    print("foo", nodeid, sessionid, data)
     for gate_node_id in cluster.cluster.gates:
         cluster.cluster.call_node(gate_node_id, "broadcast", 1, b'logic server data!!')
+        timer.add_later_task(3, cluster.cluster.call_node, gate_node_id, "sendto_session", sessionid, 123, b'ss')
+
     return "foo"
 
 
-# 定时任务
-timer.add_loop_task(2, foo, "logic foo!")
+def test_loop_task():
+    for gate_node_id in cluster.cluster.gates:
+        print('test_loop_task', gate_node_id)
+        cluster.cluster.call_node(gate_node_id, "broadcast", 1, b'logic server data!!')
+
+
+timer.add_loop_task(3, test_loop_task, now=False)
+
 server.serve_forever()
